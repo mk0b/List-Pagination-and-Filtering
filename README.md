b129960f2582c955203079186ea8996badf3932f
Megan Katherine O'Brien <megankob@gmail.com>
2019-10-22 14:12:15 -0400

    Clean Up
    Comments cleaned up and new comments added. Completed the Read.me

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # List Pagination and Filtering
  Megan Katherine O'Brien's List Pagination and Filtering Project 2 as a TeamTreeHouse Full Stack JavaScript Talent Path student.
+
+Grading Note: Going for Meets Expectations
+
+This JavaScript program takes a list of students and paginates the list into 10 records per page.
--- js/script.js
+++ js/script.js
@@ -2,60 +2,22 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+/*I started to refactor but the functions to create/append were not 
+making my code smaller just added more function code so I left my code 
+the same.*/
 
-//TODO: Delete existing comments and clean up my comments.
-//TODO: Go through and refactor code to make sure it is as concise as possible.
-//TODO: Double check progressive enhancement. Make sure core functionality can be
-//completed wihtout JavaScript turned on.
-//TODO: Test on all major browsers.
+//TODO: Fix the active button issue once I receive help.
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-//You need two global variables.
-//Store the student list items elements in the studentlist variable.
-//log out hte variable storing the list to ensure it equals the list of li items
-//and not the container of the li elements. (not sure what this means)
-//Create a variable to store the number of items to show on each "page" which is 10.
-
+//grabbing my intital elements, putting my collection into an array, and setting the records to show per page.
 const ul = document.querySelector('.student-list');
 const studentList = [...ul.children];
 const recordsPerPage = 10;
 
-//TODO: Remove before submitting.
-console.log(studentList);
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
+//function to show or hide the correct index of students on each page.
 function showPage(list, page) {
    let startIndex = (page * recordsPerPage) - recordsPerPage;
    let endIndex = page * recordsPerPage;
-
-   
 
    for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex) {
@@ -66,14 +28,11 @@ function showPage(list, page) {
    }
 }
 
+/*function to decide how many pages there should be depending on the list of students,
+add the number of button links for each page, add an event listenere and call the show page
+funtion for each event listener. 
+*/
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-//change back to a variable like they have the guide?
-//had to use math.ceil to round up because 54/10 was 5.4 and I was getting 5 pages and I needed 6.
 function addPaginationLinks (list) {
    let totalPages = Math.ceil(list.length / recordsPerPage);
    const newDiv = document.createElement('div');
@@ -91,8 +50,8 @@ function addPaginationLinks (list) {
       li.appendChild(a);
       a.textContent = i;
       a.href = '#';
-     
       let aList = document.querySelectorAll('a');
+      //adding an event listener to each link
       for (let j = 1; j <= aList.length; j++) {
          a.addEventListener('click', (event) => {
             showPage(studentList, j);
@@ -102,17 +61,7 @@ function addPaginationLinks (list) {
       }
       li.firstChild.className = 'active';
    }
-
-   
-   //Add an event listener to each a tag. When they are clicked
-   //call the showPage function to display the appropriate page
-   
-   //Loop over pagination links to remove active class from all links
-   //Add the active class to the link that was just clicked. You can
-   //identify that clicked link using event.target
 }
 
+//calling my pagination function
 addPaginationLinks(studentList);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
